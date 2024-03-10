bd6f3d12757cec500442a6db2dc3620662ec8559
oktuvida <oktuvida@hotmail.com>
2024-03-10 15:50:37 -0500

    feat: add SJF scheduler

--- src/ur_os/SJF_NP.java
+++ src/ur_os/SJF_NP.java
@@ -8,24 +8,33 @@ package ur_os;
  *
  * @author prestamour
  */
-public class SJF_NP extends Scheduler{
+public class SJF_NP extends Scheduler {
 
-    
-    SJF_NP(OS os){
+    SJF_NP(OS os) {
         super(os);
     }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        
-        
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //It's empty because it is Non-preemptive
 
     @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //It's empty because it is Non-preemptive
-    
+    public void getNext(boolean cpuEmpty) {
+        if (!processes.isEmpty() && cpuEmpty) {
+
+            Process p = processes.get(0);
+            for (int i = 1; i < processes.size(); i++) {
+                if (processes.get(i).getRemainingTimeInCurrentBurst() < p.getRemainingTimeInCurrentBurst()) {
+                    p = processes.get(i);
+                }
+            }
+            processes.remove(p);
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
+        }
+    }
+
+    @Override
+    public void newProcess(boolean cpuEmpty) {
+    } // It's empty because it is Non-preemptive
+
+    @Override
+    public void IOReturningProcess(boolean cpuEmpty) {
+    } // It's empty because it is Non-preemptive
+
 }
